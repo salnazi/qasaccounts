2206ddda7da723b2d5827a21b2d315520e3c9796
Salim Nazir <salim@11infotech.com>
2023-05-02 13:34:14 +0700

    test

---
+++ portal/accounts.php
@@ -0,0 +1,88 @@
+<div class="mobile-menu-overlay"></div>
+
+	<div class="main-container">
+        
+		<div class="pd-ltr-20">
+
+                <div class="card-box pd-20 height-100-p mb-30">
+                    <div class="row align-items-center">
+                        
+                        <div class="col-md-6">
+                        <h5 style='center;color:#343a40;'>Accounting & BookKeeping Service</h5><br>
+                            <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method = "POST" class="tab-wizard2 wizard-circle wizard">
+								
+								<section>
+									<div class="form-wrap max-width-600 mx-auto">
+										
+										<div class="form-group row">
+											<label class="col-sm-4 col-form-label">Bank Statements</label>
+											<div class="col-sm-8">
+												<input type="file" class="form-control">
+											</div>
+										</div>
+
+                                        <div class="form-group row">
+											<label class="col-sm-4 col-form-label">Invoice Receipts</label>
+											<div class="col-sm-8">
+												<input type="file" class="form-control">
+											</div>
+										</div>
+
+                                        <div class="form-group row">
+											<label class="col-sm-4 col-form-label">Expense Receipts</label>
+											<div class="col-sm-8">
+												<input type="file" class="form-control">
+											</div>
+										</div>
+
+                                        <div class="form-group row">
+											<label class="col-sm-4 col-form-label">Pay-roll Details</label>
+											<div class="col-sm-8">
+												<input type="file" class="form-control">
+											</div>
+										</div>
+
+                                        <div class="form-group row">
+											<label class="col-sm-4 col-form-label">Investment Details</label>
+											<div class="col-sm-8">
+												<input type="file" class="form-control">
+											</div>
+										</div>
+
+                                        <div class="form-group row">
+											<label class="col-sm-4 col-form-label">Loans Details</label>
+											<div class="col-sm-8">
+												<input type="file" class="form-control">
+											</div>
+										</div>
+
+                                        <div class="form-group row">
+											<label class="col-sm-4 col-form-label">I would like get</label>
+											<div class="col-sm-8">
+                                                <input type='checkbox' value = 'profit-and-loss-statement'> Profit & Loss Statement <br/>
+                                                <input type='checkbox' value = 'balance-sheet'> Balance Sheet <br/>
+                                                <input type='checkbox' value = 'cash-flow'> Cash Flow 
+											</div>
+										</div>
+
+                                        <div class="form-group row">
+											<label class="col-sm-4 col-form-label">Comment</label>
+											<div class="col-sm-8">
+                                                <textarea cols='70' rows='7'></textarea>
+											</div>
+										</div>
+
+                                </section>
+                            </form>
+
+                        </div>
+                    </div>
+                </div>
+
+			<div class="footer-wrap pd-20 mb-20 card-box">
+				<?php include("footer.php"); ?>
+			</div>
+
+		</div>
+
+	</div>
--- portal/dashboard.php
+++ portal/dashboard.php
@@ -9,16 +9,7 @@
         include("top_nav.php");
         //include("right_side_bar.php");
         include("left_side_bar.php");
-
-        if($_GET['id'] == 'accounts'){
-            echo "asdasd";
-            include("accounts.php");
-        }
-
-        if($_GET['id'] == 'dashboard'){
-            include("main_container.php");
-        }
-        //nclude("main_container.php");
+        i//nclude("main_container.php");
         include("footer_js.php");
     ?>
 </body>
